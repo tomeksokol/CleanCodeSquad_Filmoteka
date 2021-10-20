a95be2b235b8215116d3af8de7269d8267ebde7f
Tomasz Sokół <sokol.tomasz@gmail.com>
2021-10-20 16:18:48 +0200

    0
    zero-configuration

--- snowpack.config.js
+++ snowpack.config.js
@@ -6,9 +6,7 @@ module.exports = {
   mount: {
     /* ... */
   },
-  plugins: [
-    '@snowpack/plugin-sass',
-  ],
+  plugins: ["@snowpack/plugin-sass"],
   packageOptions: {
     /* ... */
   },
@@ -16,6 +14,6 @@ module.exports = {
     /* ... */
   },
   buildOptions: {
-    /* ... */
+    baseUrl: "/gh-pages-CleanCodeSquad_Filmoteka",
   },
 };
