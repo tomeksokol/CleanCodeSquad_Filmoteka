8d13529f997faa79c45c1b4e91bc8ef3e42d9308
Tomasz Sokół <sokol.tomasz@gmail.com>
2021-10-29 11:24:12 +0200

    movie modal cart

--- js/movieModalWindow.js
+++ js/movieModalWindow.js
@@ -1,11 +1,13 @@
 import { renderMovies, setFilms } from "./renderMovies.js";
 
 const galleryContainer = document.querySelector(".movie__container");
-
 const modal = document.querySelector("#myModal");
 const closeBtn = document.querySelector(".close");
 const modalContent = document.querySelector(".modal-view");
-
+const modalButtonWatched = document.querySelector(
+  'button[data-watchedId="${id}"]'
+);
+const modalButtonQueue = document.querySelector('button[data-queueID="${id}"]');
 
 //Event opening modal window
 galleryContainer.addEventListener("click", selectMovieCart);
@@ -13,8 +15,32 @@ galleryContainer.addEventListener("click", selectMovieCart);
 function selectMovieCart(event) {
   event.preventDefault();
   console.log(event.target.parentNode.parentNode);
-  console.log(`Movie ID: ${event.target.parentNode.parentNode.dataset.id}`)
+  console.log(`Movie ID: ${event.target.parentNode.parentNode.dataset.id}`);
   modal.style.display = "block";
+
+  function fetchMoiveId() {
+    let id = `${event.target.parentNode.parentNode.dataset.id}`;
+    console.log(id);
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=5d5fbc20666787ca7b4a0d9d71c08715`
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    });
+  }
+
+  function setMovieCard() {
+    fetchMoiveId()
+      .then((id) => {
+        renderMovieCart(id);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  setMovieCard();
+
   closeBtn.addEventListener("click", () => {
     modal.style.display = "none";
   });
@@ -26,3 +52,39 @@ window.addEventListener("click", (ev) => {
     modal.style.display = "none";
   }
 });
+
+function renderMovieCart(id) {
+  // let id = { id, original_title, poster_path, genre_ids, popularity, overview, vote_average, vote_count };
+  modalContent.innerHTML = `<div class="modal__cartContainer">
+  <div class="modal__poster">
+  <img class="modal__images" src="https://image.tmdb.org/t/p/w300${id.poster_path}" alt="Poster of: ${id.original_title}"/>
+  </div>
+  <div class="modal__description">
+  <h2 class="modal__header">${id.original_title}</h2>
+  <div class="modal__category">
+  <ul class="modal__list">
+  <li class="modal__list-item">Vote / Votes</li>
+  <li class="modal__list-item">Popularity</li>
+  <li class="modal__list-item">Orginal Title</li>
+  <li class="modal__list-item">Genre</li>
+  </ul>
+  </div>
+  <div class="modal__text">
+  <ul class="modal__list-text">
+  <li class="modal__list-text-item">${id.vote_average}</li>
+  <li class="modal__list-text-item">${id.vote_count}</li>
+  <li class="modal__list-text-item">${id.popularity}</li>
+  <li class="modal__list-text-item">${id.genre_ids}</li>
+  <li class="modal__list-text-item">${id}</li>
+  </ul>
+  </div>
+  <h4 class="modal__about">About</h4>
+  <p>${id.overview}</p>
+  </div>
+  <div class="modal__buttons-container">
+  <button class="modal__buttons btn-watched" type="submit" data-watched-id="${id}">Add to watched</button>
+  <button class="modal__buttons btn-queue" type="submit" data-queue-id="${id}">Add to queue</button>
+  </div>
+  </div>`;
+  console.log(modalButtonWatched.dataset.watchedId);
+}
--- js/renderMovies.js
+++ js/renderMovies.js
@@ -17,7 +17,6 @@ function setFilms() {
   fetchFilms()
     .then((movie) => {
       renderMovies(movie);
-      renderMovieCart(movie);
     })
     .catch((err) => {
       console.log(err);
@@ -70,55 +69,5 @@ function renderMovies(movie) {
     }
   );
 }
-// /////////////////////
-// const modalContent = document.querySelector(".modal-txt");
-// function renderMovieCart(movie) {
-//   movie.results.forEach(
-//     ({
-//       original_title,
-//       poster_path,
-//       release_date,
-//       genre_ids,
-//       vote_average,
-//       overview,
-//     }) => {
-//       let movieGenre = movieId
-//         .filter((genre) => genre_ids.includes(genre.id))
-//         .map((genre) => genre.name)
-//         .join(", ");
 
-//       let relaseYear = release_date.substring(0, 4);
-
-//       let markup = `
-//       <div class="modal__cart"><div class="modal__poster"><p>${overview}</p><p>${vote_average}</p>
-//       <img class="modal__image" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${original_title}"/>
-//       </div>
-//       <h1 class="modal__movie-name>${original_title}</h1>
-//       <p>${overview}</p>
-//       </div>`;
-//       modalContent.insertAdjacentHTML("beforeend", markup);
-//     }
-//   );
-// }
-// function renderMovieCart(movie) {
-//   const{poster_path, genres, vote_average, vote_count, popularity, original_title, overview} = movie;
-
-//   const markup =`<div class="moddal__window">
-//   <div class="moddal__grid">
-//   <img class="moddal__poster" src="https://image.tmdb.org/t/p/w300${poster_path}" alt="plakat" />
-//   <div>
-//   <h1 class="moddal__title">${original_title}</h1>
-//   <div class="moddal__data"><p class="moddal__dataTitle moddal__data--1">Vote / Votes</p><div class="moddal__data"><p class="moddal__dataTxt moddal__voteA">${vote_average}</p>/<p class="moddal__dataTxt moddal__voteC">${vote_count}</p></div></div>
-//   <div class="moddal__data"><p class="moddal__dataTitle moddal__data--2">Popularity</p><p class="moddal__dataTxt">${popularity}</p></div>
-//   <div class="moddal__data"><p class="moddal__dataTitle moddal__data--3">Original Title</p><p class="moddal__dataTxt">${original_title}</p></div>
-//   <div class="moddal__data"><p class="moddal__dataTitle moddal__data--4">Genre</p><p class="moddal__dataTxt">genres</p></div>
-//   <h2 class="moddal__about">ABOUT</h2>
-//   <h3 class="moddal__aboutTxt">${overview}</h3>
-//   <div class="moddal__buttons"><button class="moddal__btn">add to Watched</button><button class="moddal__btn">add to queue</button></div>
-//   </div></div></div>
-//   `
-// modalContent.insertAdjacentHTML("beforeend", markup);
-// }
-
-////////////////
 export { renderMovies, setFilms };
