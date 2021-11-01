c50f394cfc1df334280995ff231558e2f14972a3
PatrycjaNowakowska <poczta.pnowakowska@gmail.com>
2021-11-01 23:39:16 +0100

    muLibrary

--- js/renderLibrary.js
+++ js/renderLibrary.js
@@ -1,76 +1,78 @@
 import loaderToggle from "./spinner.js";
-const BASE_URL = "https://api.themoviedb.org/3/";
 const libraryContainer = document.querySelector(".library__container");
-let movieId = [];
+const watchedBtn = document.querySelector(".watched");
+const queuedBtn = document.querySelector(".queue");
 
-async function fetchFilms() {
-  const page = 1;
-  return fetch(
-    `${BASE_URL}trending/movie/week?api_key=5d5fbc20666787ca7b4a0d9d71c08715`
-  ).then((response) => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+async function fetchMovie(id) {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=5d5fbc20666787ca7b4a0d9d71c08715`
+  );
+  if (!response.ok) {
+    throw new Error(response.status);
+  }
+  return await response.json();
 }
 
-async function setFilms() {
-  loaderToggle();
-  fetchFilms()
-    .then((movie) => {
-      renderMovies(movie);
-      loaderToggle();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
-setFilms();
+function renderMovieItem(movie) {
+  const { id, poster_path, genres, original_title, release_date } = movie;
+  let relaseYear = movie.release_date.slice(0, 4);
+  let gen = genres
+    .map((genre) => genre.name)
+    .slice(0, 3)
+    .join(", ");
 
-async function fetchGenres() {
-  return fetch(
-    `${BASE_URL}genre/movie/list?api_key=5d5fbc20666787ca7b4a0d9d71c08715`
-  ).then((response) => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-}
-
-function setGenres() {
-  fetchGenres().then((id) => {
-    return (movieId = id.genres);
-  });
-}
-setGenres();
-function renderMovies(movie) {
-  movie.results.forEach(
-    ({ original_title, poster_path, release_date, genre_ids }) => {
-      let movieGenre = movieId
-        .filter((genre) => genre_ids.includes(genre.id))
-        .map((genre) => genre.name)
-        .join(", ");
-
-      let relaseYear = release_date.substring(0, 4);
-      libraryContainer.innerHTML += `<div class="movies-cart">
+  let poster = "";
+  if (poster_path) {
+    poster = `https://image.tmdb.org/t/p/w500${poster_path}`;
+  } else {
+    poster = "./images/poster-placeholder.png";
+  }
+  libraryContainer.innerHTML += `<div class="movies-cart">
       <ul class="movies-list">
-      <li class="movies-item">
+      <li class="movies-item" data-id="${id}">
       <div class="movies-poster">
-      <img class="movies-image" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${original_title}"/>
+      <img class="movies-image" src=${poster} alt="${original_title}"/>
+      <div class="middle">
+      <div><img class="movies-preview" src="./images/eye.svg"/></div>
+      </div>
       </div>
 
   <div class="description">
     <h2 class="description-title">${original_title}</h2>
     <p class="description-container">
-      <span class="description-info">${movieGenre} |
+      <span class="description-info"> ${gen} |
       ${relaseYear}</span>
     </p>
   </div>
 </li>
 </ul>
 </div>`;
-    }
-  );
 }
+
+function renderMovieID(id) {
+  loaderToggle();
+  fetchMovie(id).then((movie) => {
+    renderMovieItem(movie);
+    loaderToggle();
+  });
+}
+
+function getwatchedMovies() {
+  libraryContainer.innerHTML = "";
+  const savedWatchedMovies = localStorage.getItem("watchedMovieIDs");
+  let watchedMovies = JSON.parse(savedWatchedMovies);
+  //console.log(watchedMovies);
+  watchedMovies.forEach(renderMovieID);
+}
+
+watchedBtn.addEventListener("click", getwatchedMovies);
+
+const savedQueuedMovies = localStorage.getItem("queuedMovieIDs");
+function getQueuedMovies() {
+  libraryContainer.innerHTML = "";
+  let queuedMovies = JSON.parse(savedQueuedMovies);
+  //console.log(queuedMovies);
+  queuedMovies.forEach(renderMovieID);
+}
+
+queuedBtn.addEventListener("click", getQueuedMovies);
